ed41b9f80cce872efc2605ffabf436810aa7d007
Mark Smukler <smukler.mark@gmail.com>
2015-08-24 12:44:56 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+var application_root = __dirname,
+    express          = require('express'),
+    bodyParser       = require('body-parser'),
+    path             = require('path'),
+    logger           = require('morgan'),
+    models           = require('./models');
+
+var app = express();
+
+// Server Configuration
+app.use( logger('dev') );
+// set up request parsing
+app.use( bodyParser.urlencoded({ extended: false }) );
+app.use( bodyParser.json() );
+// set up method override for PUT and DELETE
+app.use( methodOverride() );
+// set up serving of static assets
+app.use( express.static( path.join( application_root, 'public' ) ) );
+app.use( express.static( path.join( application_root, 'browser' ) ) );
+
+// Routes
+
+// Export app as module
+module.exports = app;
